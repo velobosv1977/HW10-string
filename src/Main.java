c6a04d96b818fcbaf1c88c4f63f32049a1621b0a
velobosv1977 <velobosv33@gmail.com>
2024-12-02 13:26:42 +0300

    Initial Commit HW-10

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // HomeWork 10. String & method
+        System.out.println("Домашняя работ 10. Строки и методы");
+        System.out.println("");
+        //Task 1.
+        System.out.println("Задача 1. СТроки и методы");
+
+
+        );
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
